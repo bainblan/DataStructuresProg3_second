f5ffdba7b19fd58159aa347e967590f9a4250069
Baines <brb56383@uga.edu>
2025-10-26 16:48:30 -0400

    just checking the commit works

--- BSTDriver.java
+++ BSTDriver.java
@@ -2,7 +2,6 @@
 public class BSTDriver {
     public static void main(String[] args) {
         BinarySearchTree<Integer> bst = new BinarySearchTree<>();
-
         // Add test cases here to test the BinarySearchTree methods
         bst.insert(9);
         bst.insert(4);
@@ -10,7 +9,6 @@ public class BSTDriver {
         bst.insert(5);
         bst.insert(15);
         bst.insert(13);
-        
         bst.inOrder();
         System.out.println();
         bst.getCousins(bst.getRoot().getRight().getLeft()); // Example node
